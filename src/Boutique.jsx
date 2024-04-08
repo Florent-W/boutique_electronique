b845c3f0093951f56873e37ed84a0c4b908ea481
Florent-W <35995337+Florent-W@users.noreply.github.com>
2024-04-08 11:05:53 +0200

    Lancement du projet

---
+++ src/Boutique.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Boutique() {
+    return (
+      <div className="Boutique">
+         
+      </div>
+    );
+  }
+  
+  export default Boutique;
