2249e5de8f5dbd664598684591d49301e98ee852
gourdinax <gourdinax@yahoo.fr>
2024-04-09 11:09:06 +0200

    page article

--- src/app/routers/default.tsx
+++ src/app/routers/default.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../../pages/shop/home.page";
-
+import ArticlePage from "../../pages/shop/article.page"; 
 export default function DefaultRouter() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/article/:id" element={<ArticlePage />} /> 
     </Routes>
   );
 }
