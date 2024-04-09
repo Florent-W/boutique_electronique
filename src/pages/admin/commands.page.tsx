de706def13fc9cae883c11a650c66dd891b31677
dantin-durand <durand.dantin.pro@gmail.com>
2024-04-10 01:28:57 +0200

    [fix] add dayjs

--- src/pages/admin/commands.page.tsx
+++ src/pages/admin/commands.page.tsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Order, useOrder } from '../../app/contexts/order.context';
-import { getOrders } from '../../api/order';
-import Layout from '../../components/Layout';
-import BackButton from '../../components/BackButton';
-import moment from 'moment-timezone';
-import 'moment/locale/fr';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Order, useOrder } from "../../app/contexts/order.context";
+import { getOrders } from "../../api/order";
+import Layout from "../../components/Layout";
+import BackButton from "../../components/BackButton";
+import moment from "moment-timezone";
+import "moment/locale/fr";
 
 export const CommandsPage = () => {
   const navigate = useNavigate();
   const { order } = useOrder();
   const [ordersData, setOrdersData] = useState<Order[]>([]);
 
-  moment.locale('fr');
+  moment.locale("fr");
 
   const fetchOrders = async () => {
     try {
@@ -33,7 +33,7 @@ export const CommandsPage = () => {
   };
 
   const formatDate = (dateString: moment.MomentInput) => {
-    return moment(dateString).tz('UTC').format('D MMMM YYYY à HH:mm:ss');
+    return moment(dateString).tz("UTC").format("D MMMM YYYY à HH:mm:ss");
   };
 
   useEffect(() => {
@@ -45,26 +45,46 @@ export const CommandsPage = () => {
       <BackButton />
       <div className="flex justify-center mt-10">
         <div className="w-full max-w-4xl bg-gradient-to-r from-red-800 to-red-500 rounded-xl shadow-2xl p-8 mb-10">
-          <h1 className="text-center text-4xl font-bold text-white mb-10">Gestion des commandes</h1>
+          <h1 className="text-center text-4xl font-bold text-white mb-10">
+            Gestion des commandes
+          </h1>
           <div className="bg-white p-6 rounded-xl shadow-md">
             <div className="overflow-x-auto">
               <table className="min-w-full leading-normal">
                 <thead>
                   <tr>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider rounded-tl-xl">Id</th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Date</th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Quantité</th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Statut</th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider rounded-tr-xl">Actions</th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider rounded-tl-xl">
+                      Id
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      Date
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      Quantité
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      Statut
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider rounded-tr-xl">
+                      Actions
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
                   {ordersData.map((order) => (
                     <tr key={order.id}>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{order.id}</td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{formatDate(order.date)}</td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{order.totalAmount}</td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{order.status}</td>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        {order.id}
+                      </td>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        {formatDate(order.date)}
+                      </td>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        {order.totalAmount}
+                      </td>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        {order.status}
+                      </td>
                       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm flex justify-end items-center">
                         <button
                           onClick={() => handleEdit(order.id)}
