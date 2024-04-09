5fbb9f13f7cf8886dd48adf103241557d120d053
dantin-durand <durand.dantin.pro@gmail.com>
2024-04-09 12:22:03 +0200

    [func] add products list

--- src/pages/shop/home.page.tsx
+++ src/pages/shop/home.page.tsx
@@ -4,15 +4,18 @@ import { Link } from "react-router-dom";
 import { RiAccountCircleLine, RiShoppingCartLine } from "@remixicon/react";
 import Navbar from "../../components/Navbar";
 import { getProducts } from "../../api/products";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
+  const [products, setProducts] = useState([]);
   const { user } = useUser();
   console.log(user);
+
   const fetchProducts = async () => {
     try {
       const response = await getProducts();
       console.log(response);
+      setProducts(response);
     } catch (error) {
       console.log(error);
     }
@@ -21,9 +24,15 @@ export default function HomePage() {
   useEffect(() => {
     fetchProducts();
   }, []);
+
   return (
     <div className="">
       <Navbar />
+      {products.map((product: any) => (
+        <div>
+          <p>{product?.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
